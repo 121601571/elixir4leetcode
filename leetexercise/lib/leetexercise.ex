75390f8c53cbbff932b1ce53f7b6fc63ad0560b5
Alex Li <yili6@microsoft.com>
2022-11-04 15:16:54 +0800

    init

---
+++ leetexercise/lib/leetexercise.ex
@@ -0,0 +1,18 @@
+defmodule Leetexercise do
+  @moduledoc """
+  Documentation for `Leetexercise`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Leetexercise.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ leetexercise/test/leetexercise_test.exs
@@ -0,0 +1,8 @@
+defmodule LeetexerciseTest do
+  use ExUnit.Case
+  doctest Leetexercise
+
+  test "greets the world" do
+    assert Leetexercise.hello() == :world
+  end
+end
